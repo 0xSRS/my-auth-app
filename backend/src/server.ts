5ada3198a17a23ec25907157d1a96650839a7f19
Suryansh <suryanshrajsingh10405@gmail.com>
2026-03-30 18:26:34 +0530

    fixed types for env variable

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,9 +16,9 @@ app.use("/api/auth",authRoutes)
 
 async function start(){
     try{
-        await mongoose.connect(process.env.DB_HOST)
-        app.listen(process.env.PORT,()=>{
-            console.log(`Listening at port ${process.env.PORT}`)
+        await mongoose.connect(process.env.DB_HOST!)
+        app.listen(process.env.PORT!,()=>{
+            console.log(`Listening at port ${process.env.PORT!}`)
         })
     }catch{
         console.log("Unable to connect to DB")
